a944a6baa36215c410bb0b1f8b5502cabe374a0b
Alex Olkhovskiy <lxol@users.noreply.github.com>
2020-04-16 14:03:14 +0100

    [DC-2411] Enable replies for non P800 messages

--- test/uk/gov/hmrc/twowaymessage/services/HtmlCreatorServiceSpec.scala
+++ test/uk/gov/hmrc/twowaymessage/services/HtmlCreatorServiceSpec.scala
@@ -57,7 +57,7 @@ class HtmlCreatorServiceSpec extends PlaySpec with GuiceOneAppPerSuite with Fixt
 
   val latestMessageId = "5d02201b5b0000360151779e"
 
-  val listOfConversationItems = List(
+  def listOfConversationItems(enquiryType: String) = List(
     ConversationItem(
       "5d02201b5b0000360151779e",
       "Matt Test 1",
@@ -67,7 +67,7 @@ class HtmlCreatorServiceSpec extends PlaySpec with GuiceOneAppPerSuite with Fixt
           FormId.Reply,
           Some(LocalDate.parse("2019-06-13")),
           Some("5d021fbe5b0000200151779c"),
-          Some("P800"))),
+          Some(enquiryType))),
       LocalDate.parse("2019-06-13"),
       Some(
         "<p>Dear TestUser</p><p>Thank you for your message of 13 June 2019.</p><p>To recap your question, " +
@@ -84,20 +84,20 @@ class HtmlCreatorServiceSpec extends PlaySpec with GuiceOneAppPerSuite with Fixt
           FormId.Question,
           Some(LocalDate.parse("2019-06-13")),
           None,
-          Some("p800"))),
+          Some(enquiryType))),
       LocalDate.parse("2019-06-13"),
       Some("<p>Hello&nbsp;this&nbsp;is&nbsp;a&nbsp;test!</p>")
     )
   )
 
   "createConversation" should {
-    "create HTML for a customer" in {
+    "create HTML for a customer and p800 enquiryType" in {
       when(
         mockTwoWayMessageService
           .findMessagesBy(any[String])(any[HeaderCarrier]))
-        .thenReturn(Future.successful(Right(listOfConversationItems)))
+        .thenReturn(Future.successful(Right(listOfConversationItems("p800"))))
       val result =
-        await(htmlCreatorService.createConversation(latestMessageId, listOfConversationItems, RenderType.CustomerLink))
+        await(htmlCreatorService.createConversation(latestMessageId, listOfConversationItems("p800"), RenderType.CustomerLink))
       result mustBe
         Right(
           Html.apply(
@@ -118,13 +118,56 @@ class HtmlCreatorServiceSpec extends PlaySpec with GuiceOneAppPerSuite with Fixt
           <div><p>Hello&#160;this&#160;is&#160;a&#160;test!</p></div>)))
     }
 
+    "create HTML for a customer and epaye-general enquiryType" in {
+      when(
+        mockTwoWayMessageService
+          .findMessagesBy(any[String])(any[HeaderCarrier]))
+        .thenReturn(Future.successful(Right(listOfConversationItems("epaye-general"))))
+      val result =
+        await(htmlCreatorService.createConversation(latestMessageId, listOfConversationItems("epaye-general"), RenderType.CustomerLink))
+      result mustBe
+        Right(
+          Html.apply(
+            Xhtml.toXhtml(<h1 class="govuk-heading-xl margin-top-small margin-bottom-small">Matt Test 1</h1>
+          <p class="faded-text--small">This message was sent to you on 13 June 2019</p> ++
+              Utility.trim(<div>
+              <p>Dear TestUser</p>
+              <p>Thank you for your message of 13 June 2019.</p>
+              <p>To recap your question, I think you're asking for help with</p>
+              <p>I believe this answers your question and hope you are satisfied with the response.</p>
+              <p>If you think there is something important missing, use the link at the end of this message to find out how to contact HMRC.</p>
+              <p>Regards<br/>Matthew Groom<br/>HMRC digital team</p>
+            </div>) ++
+
+          Utility.trim(
+            <p>
+              <span>
+                <a style="text-decoration:none;" href="/two-way-message-frontend/message/customer/epaye-general/5d02201b5b0000360151779e/reply#reply-input-label">
+                  <svg style="vertical-align:text-top;padding-right:5px;" width="21px" height="20px" viewBox="0 0 33 31" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                    <title>Reply</title>
+                    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                      <g id="icon-reply" fill="#000000" fill-rule="nonzero">
+                        <path d="M20.0052977,9.00577935 C27.0039418,9.21272548 32.6139021,14.9512245 32.6139021,22 C32.6139021,25.5463753 31.1938581,28.7610816 28.8913669,31.1065217 C29.2442668,30.1082895 29.4380446,29.1123203 29.4380446,28.1436033 C29.4380446,21.8962314 25.9572992,21.1011463 20.323108,21 L15,21 L15,30 L-1.42108547e-14,15 L15,2.25597319e-13 L15,9 L20,9 L20.0052977,9.00577935 Z" id="Combined-Shape"></path>
+                      </g>
+                    </g>
+                  </svg>
+                </a>
+              </span>
+              <a href="/two-way-message-frontend/message/customer/epaye-general/5d02201b5b0000360151779e/reply#reply-input-label">Send another message about this</a>
+            </p>) ++
+          <hr/>
+          <h2 class="govuk-heading-xl margin-top-small margin-bottom-small">Matt Test 1</h2>
+          <p class="faded-text--small">You sent this message on 13 June 2019</p>
+          <div><p>Hello&#160;this&#160;is&#160;a&#160;test!</p></div>)))
+    }
+
     "create HTML content for an advisor" in {
       when(
         mockTwoWayMessageService
           .findMessagesBy(any[String])(any[HeaderCarrier]))
-        .thenReturn(Future.successful(Right(listOfConversationItems)))
+        .thenReturn(Future.successful(Right(listOfConversationItems("p800"))))
       val result =
-        await(htmlCreatorService.createConversation(latestMessageId, listOfConversationItems, RenderType.Adviser))
+        await(htmlCreatorService.createConversation(latestMessageId, listOfConversationItems("p800"), RenderType.Adviser))
       result mustBe
         Right(
           Html(
@@ -199,7 +242,7 @@ class HtmlCreatorServiceSpec extends PlaySpec with GuiceOneAppPerSuite with Fixt
     "create a complete HTML document" in {
       val subject = "Some subject"
       val result = await(
-        htmlCreatorService.createHtmlForPdf(latestMessageId, "AB234567C", listOfConversationItems, "Some subject", enquiries("P800").get))
+        htmlCreatorService.createHtmlForPdf(latestMessageId, "AB234567C", listOfConversationItems("p800"), "Some subject", enquiries("P800").get))
       result mustBe
         Right(expectedPdfHtml(subject, "Received from: P800 secure message", "National insurance number"))
     }
@@ -207,7 +250,7 @@ class HtmlCreatorServiceSpec extends PlaySpec with GuiceOneAppPerSuite with Fixt
     "correctly render escaped HTML in the message subject" in {
       val result = await(
         htmlCreatorService
-          .createHtmlForPdf(latestMessageId, "AB234567C", listOfConversationItems, subjectWithEscapedChars, enquiries("P800").get))
+          .createHtmlForPdf(latestMessageId, "AB234567C", listOfConversationItems("p800"), subjectWithEscapedChars, enquiries("P800").get))
       result mustBe
         Right(expectedPdfHtml(subjectWithEscapedChars, "Received from: P800 secure message", "National insurance number"))
     }
@@ -216,7 +259,7 @@ class HtmlCreatorServiceSpec extends PlaySpec with GuiceOneAppPerSuite with Fixt
       val subjectWithUnescapedChars = "<b>This is another test to see if this > that & that < this</b>"
       val result = await(
         htmlCreatorService
-          .createHtmlForPdf(latestMessageId, "AB234567C", listOfConversationItems, subjectWithUnescapedChars, enquiries("P800").get))
+          .createHtmlForPdf(latestMessageId, "AB234567C", listOfConversationItems("p800"), subjectWithUnescapedChars, enquiries("P800").get))
       result mustBe
         Right(expectedPdfHtml(subjectWithEscapedChars, "Received from: P800 secure message", "National insurance number"))
     }
