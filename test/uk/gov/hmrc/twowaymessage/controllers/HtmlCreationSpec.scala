56ee6529272ffbea5acf079a5c929e24c49e0b93
Alex Olkhovskiy <lxol@users.noreply.github.com>
2020-04-16 14:54:23 +0100

    [DC-2411] Enable replies for non P800 messages (#120)

--- test/uk/gov/hmrc/twowaymessage/controllers/HtmlCreationSpec.scala
+++ test/uk/gov/hmrc/twowaymessage/controllers/HtmlCreationSpec.scala
@@ -75,7 +75,7 @@ class HtmlCreationSpec extends TestUtil with MockAuthConnector {
     FakeHeaders(),
     twoWayMessageGood)
 
-  val listOfConversationItems = List(
+  def listOfConversationItems(enquiryType: String) = List(
     ConversationItem(
       id = "5d02201b5b0000360151779e",
       "Matt Test 1",
@@ -85,7 +85,7 @@ class HtmlCreationSpec extends TestUtil with MockAuthConnector {
           FormId.Reply,
           Some(LocalDate.parse("2019-06-13")),
           Some("5d021fbe5b0000200151779c"),
-          Some("P800"))),
+          Some(enquiryType))),
       LocalDate.parse("2019-06-13"),
       Some(Base64.getEncoder.encodeToString(
         "Dear TestUser<br>Thank you for your message of 13 June 2019.<br>To recap your question, I think you're asking for help with<br>I believe this answers your question and hope you are satisfied with the response.<br>If you think there is something important missing, use the link at the end of this message to find out how to contact HMRC.<br/>Regards<br/>Matthew Groom<br/>HMRC digital team.".getBytes))
@@ -99,7 +99,7 @@ class HtmlCreationSpec extends TestUtil with MockAuthConnector {
           FormId.Question,
           Some(LocalDate.parse("2019-06-13")),
           None,
-          Some("p800"))),
+          Some(enquiryType))),
       LocalDate.parse("2019-06-13"),
       Some(Base64.getEncoder.encodeToString("Hello, my friend!".getBytes))
     )
@@ -117,7 +117,7 @@ class HtmlCreationSpec extends TestUtil with MockAuthConnector {
       when(
         mockMessageConnector
           .getMessages(any[String])(any[HeaderCarrier]))
-        .thenReturn(Future.successful(HttpResponse(Http.Status.OK, Some(Json.toJson(listOfConversationItems)))))
+        .thenReturn(Future.successful(HttpResponse(Http.Status.OK, Some(Json.toJson(listOfConversationItems("p800"))))))
 
       val result =
         testTwoWayMessageController.getContentBy("5d02201b5b0000360151779e", "Adviser")(fakeRequest1).run()
@@ -136,7 +136,7 @@ class HtmlCreationSpec extends TestUtil with MockAuthConnector {
         <div>Hello, my friend!</div>)
     }
 
-    "return 200 (OK) with the content of the conversation in html for the customer" in {
+    "return 200 (OK) with the content of the conversation in html for the customer and p800 enquiryType" in {
       val nino = Nino("AB123456C")
       mockAuthorise(AuthProviders(GovernmentGateway, PrivilegedApplication))(Future.successful())
       when(
@@ -147,7 +147,7 @@ class HtmlCreationSpec extends TestUtil with MockAuthConnector {
       when(
         mockMessageConnector
           .getMessages(any[String])(any[HeaderCarrier]))
-        .thenReturn(Future.successful(HttpResponse(Http.Status.OK, Some(Json.toJson(listOfConversationItems)))))
+        .thenReturn(Future.successful(HttpResponse(Http.Status.OK, Some(Json.toJson(listOfConversationItems("p800"))))))
 
       val result =
         testTwoWayMessageController.getContentBy("5d02201b5b0000360151779e", "Customer")(fakeRequest1).run()
@@ -163,6 +163,54 @@ class HtmlCreationSpec extends TestUtil with MockAuthConnector {
             Matthew Groom<br/>
             HMRC digital team.</div>) ++
           <a href="https://www.gov.uk/government/organisations/hm-revenue-customs/contact/income-tax-enquiries-for-individuals-pensioners-and-employees" target="_blank" rel="noopener noreferrer">Contact HMRC (opens in a new window or tab)</a>
+          <hr/>
+          <h2 class="govuk-heading-xl margin-top-small margin-bottom-small">Matt Test 1</h2>
+          <p class="faded-text--small">You sent this message on 13 June 2019</p>
+          <div>Hello, my friend!</div>)
+    }
+
+    "return 200 (OK) with the content of the conversation in html for the customer and epaye-general enquiryType" in {
+      val nino = Nino("AB123456C")
+      mockAuthorise(AuthProviders(GovernmentGateway, PrivilegedApplication))(Future.successful())
+      when(
+        mockMessageService.postCustomerReply(any[TwoWayMessageReply], ArgumentMatchers.eq("replyTo"))(
+          any[HeaderCarrier]))
+        .thenReturn(Future.successful(Created(Json.toJson("id" -> UUID.randomUUID().toString))))
+
+      when(
+        mockMessageConnector
+          .getMessages(any[String])(any[HeaderCarrier]))
+          .thenReturn(Future.successful(HttpResponse(Http.Status.OK, Some(Json.toJson(listOfConversationItems("epaye-general"))))))
+
+      val result =
+        testTwoWayMessageController.getContentBy("5d02201b5b0000360151779e", "Customer")(fakeRequest1).run()
+      status(result) mustBe Status.OK
+      contentAsString(result) mustBe Xhtml.toXhtml(
+        <h1 class="govuk-heading-xl margin-top-small margin-bottom-small">Matt Test 1</h1>
+          <p class="faded-text--small">This message was sent to you on 13 June 2019</p> ++
+          Utility.trim(<div>Dear TestUser<br/>Thank you for your message of 13 June 2019.<br/>
+            To recap your question, I think you're asking for help with<br/>
+            I believe this answers your question and hope you are satisfied with the response.<br/>
+            If you think there is something important missing, use the link at the end of this message to find out how to contact HMRC.<br/>
+            Regards<br/>
+            Matthew Groom<br/>
+            HMRC digital team.</div>) ++
+          Utility.trim(
+            <p>
+              <span>
+                <a style="text-decoration:none;" href="/two-way-message-frontend/message/customer/epaye-general/5d02201b5b0000360151779e/reply#reply-input-label">
+                  <svg style="vertical-align:text-top;padding-right:5px;" width="21px" height="20px" viewBox="0 0 33 31" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                    <title>Reply</title>
+                    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                      <g id="icon-reply" fill="#000000" fill-rule="nonzero">
+                        <path d="M20.0052977,9.00577935 C27.0039418,9.21272548 32.6139021,14.9512245 32.6139021,22 C32.6139021,25.5463753 31.1938581,28.7610816 28.8913669,31.1065217 C29.2442668,30.1082895 29.4380446,29.1123203 29.4380446,28.1436033 C29.4380446,21.8962314 25.9572992,21.1011463 20.323108,21 L15,21 L15,30 L-1.42108547e-14,15 L15,2.25597319e-13 L15,9 L20,9 L20.0052977,9.00577935 Z" id="Combined-Shape"></path>
+                      </g>
+                    </g>
+                  </svg>
+                </a>
+              </span>
+              <a href="/two-way-message-frontend/message/customer/epaye-general/5d02201b5b0000360151779e/reply#reply-input-label">Send another message about this</a>
+            </p>) ++
           <hr/>
           <h2 class="govuk-heading-xl margin-top-small margin-bottom-small">Matt Test 1</h2>
           <p class="faded-text--small">You sent this message on 13 June 2019</p>
